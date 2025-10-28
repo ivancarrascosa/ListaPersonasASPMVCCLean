7c49c33cf999fcacf21937cf833d255b56e84fb0
ivancarrascosa <ivan.carrascosa@iesnervion.es>
2025-10-28 16:58:57 +0100

    Inicio de ejercicio

---
+++ EjercicioListaPersonasClean/Domain/UseCase/DefaultGetListaPersonasUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UseCase
+{
+    public class DefaultGetListaPersonasUseCase : IGetListaPersonasUseCase
+    {
+        public Persona[] getListaPersonas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
